31ec79aa5b01ab3c09ca3977776ba64dd153e695
Flareon19 <aparna.rani_iot18@gla.ac.in>
2020-02-24 09:11:05 +0530

    create the fields

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -1,6 +1,13 @@
 package definition;
 
+import java.util.ArrayList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Long> phoneNumber;
+    private String email;
+
 
 
 }
