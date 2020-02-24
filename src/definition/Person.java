40fbe947cdde71887ec0ae6b990e2c4870b03d74
Flareon19 <aparna.rani_iot18@gla.ac.in>
2020-02-24 09:23:32 +0530

    define compareTo method

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -34,6 +34,6 @@ public class Person implements Comparable<Person>{
 
     @Override
     public int compareTo(Person o) {
-        return 0;
+        return this.getFirstName().compareTo(o.getFirstName());
     }
 }
