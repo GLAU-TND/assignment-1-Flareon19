d7935c5009a3b7a7437f9b1074c5b40f0e6b7141
Flareon19 <aparna.rani_iot18@gla.ac.in>
2020-02-24 10:57:24 +0530

    create addContact method

--- src/definition/EntryHelper.java
+++ src/definition/EntryHelper.java
@@ -1,5 +1,36 @@
 package definition;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EntryHelper {
+
+    public boolean addContact(){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Long> contactNumber = new ArrayList<>();
+        String flag = "y";
+        System.out.println("Please enter the name of the Person");
+        System.out.print("First Name: ");
+        String firstName = scanner.next();
+        System.out.print("Last Name: ");
+        String lastName = scanner.next();
+        while (flag.equals("y")) {
+            System.out.println("Contact Number: ");
+            contactNumber.add(scanner.nextLong());
+            System.out.println("Would you like to add another contact number? (y/n): ");
+            flag = scanner.next();
+        }
+        System.out.println("Would you like to add email address? (y/n): ");
+        flag = scanner.next();
+        scanner.nextLine();
+        if (flag.equals("y")){
+            Person person = new Person(firstName,lastName,contactNumber,scanner.nextLine());
+            scanner.close();
+            return true;
+        }
+        Person person = new Person(firstName, lastName, contactNumber, null);
+        scanner.close();
+        return true;
+    }
 
 }
