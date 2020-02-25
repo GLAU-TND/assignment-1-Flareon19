ccebc0539362b2973a28ccd998ba3a3b85415f3f
Flareon19 <aparna.rani_iot18@gla.ac.in>
2020-02-25 21:18:55 +0530

    create getter methods

--- src/definition/ContactList.java
+++ src/definition/ContactList.java
@@ -27,5 +27,12 @@ public class ContactList<Person> implements ListADT<Person> {
         private Node<E> next;
         private E data;
 
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public E getData() {
+            return data;
+        }
     }
 }
