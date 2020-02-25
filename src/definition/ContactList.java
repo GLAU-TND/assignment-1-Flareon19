a45dd267a72ee28baf0256295c8de1b8c3b843af
Flareon19 <aparna.rani_iot18@gla.ac.in>
2020-02-25 21:13:17 +0530

    make the class implement ListADT

--- src/definition/ContactList.java
+++ src/definition/ContactList.java
@@ -1,4 +1,25 @@
 package definition;
 
-public class ContactList<Person> {
+import adt.ListADT;
+
+public class ContactList<Person> implements ListADT<Person> {
+    @Override
+    public void add(Person element) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public Person remove(Person element) {
+        return null;
+    }
+
+    @Override
+    public int search(Person element) {
+        return 0;
+    }
 }
